1a7b567c0acabacb1fd6bbf2e0069cf9255b582e
vehktaur <victorakhihiero@gmail.com>
2024-10-09 17:32:35 +0100

    update brands

--- src/components/manage-brands.tsx
+++ src/components/manage-brands.tsx
@@ -1,11 +1,12 @@
 import { useBrands, useHotels } from '../hooks/hooks';
-
 import AddBrand from './add-brand';
 
+// Component for managing hotel brands
 const ManageBrands = () => {
-  const { brands, dispatchBrands } = useBrands();
-  const { dispatchHotels } = useHotels();
+  const { brands, dispatchBrands } = useBrands(); // Fetch brands and dispatch function from the custom hook
+  const { dispatchHotels } = useHotels(); // Fetch dispatch function for hotels
 
+  // Function to delete a brand and remove it from associated hotels
   const deleteBrand = (brand: string) => {
     dispatchBrands({ type: 'delete', brand });
     dispatchHotels({ type: 'removeBrandFromHotels', brand });
@@ -17,11 +18,13 @@ const ManageBrands = () => {
         <h2 className="font-semibold ~text-2xl/4xl mb-6">Manage Brands</h2>
 
         <div className="mt-2 mb-5" id="add-brand">
+          {/* Component for adding a new brand */}
           <AddBrand />
         </div>
 
         <h3 className="~text-xl/lg font-medium ~mb-2/3">All Brands</h3>
         <ol className="list-decimal ps-4 ~text-sm/base">
+          {/* List of all brands with remove buttons */}
           {brands.map((brand: string) => (
             <li key={brand} className=" py-2 ps-2 border-b">
               {brand}
