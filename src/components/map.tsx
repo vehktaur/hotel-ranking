952f6b96604791d4c20435900b133a90ad1b96e9
vehktaur <victorakhihiero@gmail.com>
2024-10-10 11:56:45 +0100

    update maps

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,30 +1,20 @@
-// import { LatLngExpression } from 'leaflet';
-// import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { setKey, fromAddress } from 'react-geocode';
-// import 'leaflet/dist/leaflet.css';
-// import markerIconPng from 'leaflet/dist/images/marker-icon.png';
-// import { Icon } from 'leaflet';
 import { useEffect, useState } from 'react';
 import {
+  AdvancedMarker,
   APIProvider,
-  ControlPosition,
   Map,
-  MapControl,
-  Marker,
   useApiIsLoaded
 } from '@vis.gl/react-google-maps';
 
 // Map component for rendering a map based on address
 const CustomMap = ({ address, name }: { address: string; name: string }) => {
-  // State to store the map position based on geocoding results
-  // const [position, setPosition] = useState<LatLngExpression | undefined>(
-  //   undefined
-  // );
   const [center, setCenter] = useState<google.maps.LatLngLiteral | undefined>();
   const apiIsLoaded = useApiIsLoaded();
 
   // Set API key for geocoding service
   const apiKey = import.meta.env.VITE_MAP_API_KEY;
+  const mapId = import.meta.env.VITE_MAP_ID;
   setKey(apiKey || '');
 
   // Fetch and set the position using the provided address
@@ -51,25 +41,6 @@ const CustomMap = ({ address, name }: { address: string; name: string }) => {
 
   return (
     <div className="h-[30rem] max-w-3xl mx-auto rounded-xl overflow-hidden">
-      {/* <MapContainer center={position} zoom={18} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker
-          position={position!}
-          icon={
-            new Icon({
-              iconUrl: markerIconPng,
-              iconSize: [25, 41],
-              iconAnchor: [12, 41]
-            })
-          }
-        >
-          <Popup>{name}</Popup>
-        </Marker>
-      </MapContainer> */}
-
       <APIProvider apiKey={apiKey} language="en">
         <Map
           style={{ width: '100%', height: '100%' }}
@@ -77,9 +48,9 @@ const CustomMap = ({ address, name }: { address: string; name: string }) => {
           defaultZoom={18}
           gestureHandling={'greedy'}
           disableDefaultUI={true}
+          mapId={mapId}
         >
-          <Marker position={center} />
-          <MapControl position={ControlPosition.TOP_LEFT}>{name}</MapControl>
+          <AdvancedMarker position={center} title={name} />
         </Map>
       </APIProvider>
     </div>
