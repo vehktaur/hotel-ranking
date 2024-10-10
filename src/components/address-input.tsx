bb141b3268de18aa61d54553dc393d48b47cc217
vehktaur <victorakhihiero@gmail.com>
2024-10-10 21:19:42 +0100

    fix address form bug

--- src/components/address-input.tsx
+++ src/components/address-input.tsx
@@ -1,11 +1,12 @@
 import { APIProvider, useMapsLibrary } from '@vis.gl/react-google-maps';
 import { useEffect, useRef } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { Controller, useFormContext } from 'react-hook-form';
 
 const AddressInput = () => {
   const apiKey = import.meta.env.VITE_MAP_API_KEY;
 
   return (
+    // Wrap input with API provider to use google API functionality
     <APIProvider apiKey={apiKey}>
       <Input />
     </APIProvider>
@@ -13,33 +14,44 @@ const AddressInput = () => {
 };
 
 const Input = () => {
-  const { register, setValue } = useFormContext();
+  const { control, setValue } = useFormContext();
 
-  const placesLib = useMapsLibrary('places');
-  const addressRef = useRef<HTMLInputElement>(null);
+  const placesLibrary = useMapsLibrary('places');
+  const addressInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    if (!placesLib || !addressRef.current) return;
+    // Ensure places library is loaded and input reference is valid
+    if (!placesLibrary || !addressInputRef.current) return;
 
+    // Initialize the Autocomplete widget on the input field
     const autocompleteInstance = new google.maps.places.Autocomplete(
-      addressRef.current
+      addressInputRef.current
     );
 
+    // Listener for when a place is selected from the dropdown
     autocompleteInstance.addListener('place_changed', () => {
       const place = autocompleteInstance.getPlace();
       if (place && place.formatted_address) {
         setValue('address', place.formatted_address); // Update input with selected address
       }
     });
-  }, [placesLib]);
+  }, [placesLibrary]); // Run effect when places library is ready
 
   return (
-    <input
-      id="address"
-      type="text"
-      {...register('address', { required: true })}
-      ref={addressRef}
-      placeholder="e.g., 123 High Street"
+    // Use Controller to manage form state for the input field
+    <Controller
+      name="address"
+      control={control}
+      rules={{ required: true }}
+      render={({ field }) => (
+        <input
+          id="address"
+          type="text"
+          {...field}
+          ref={addressInputRef}
+          placeholder="e.g., 123 High Street"
+        />
+      )}
     />
   );
 };
